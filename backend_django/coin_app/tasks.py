b117b1d0b0418f8886f6522fb8284665978763dc
BuchkoN <wampase@gmail.com>
2022-12-22 13:46:58 +0700

    accelerated update courses task

--- backend_django/coin_app/tasks.py
+++ backend_django/coin_app/tasks.py
@@ -6,6 +6,11 @@ from celery import shared_task
 def update_courses():
     with open('coins.txt', encoding="utf-8") as coins:
         response = request('GET', f'https://api.binance.com/api/v3/ticker/price?symbols={coins.read()}')
-    coins_courses = response.json()
-    for coin in sorted(coins_courses, key=lambda x: x['symbol']):
-        Coin.objects.filter(name=coin['symbol'].replace('USDT', '', 1)).update(price=coin['price'])
+    if response.status_code == 200:
+        coins_courses = sorted(response.json(), key=lambda x: x['symbol'])
+        coins = list(Coin.objects.all())
+        for index in range(len(coins)):
+            coins[index].price = coins_courses[index]['price']
+        Coin.objects.bulk_update(coins, ['price'])
+    else:
+        print('API Binance не отвечает')

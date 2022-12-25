6e0661d5b570f98b165e590be46757bf5c53ab70
BuchkoN <wampase@gmail.com>
2022-12-25 13:46:13 +0700

    correction typo

--- telegram_bot/bot/settings.py
+++ telegram_bot/bot/settings.py
@@ -1,4 +1,3 @@
-import aiohttp
 TOKEN = '5961669583:AAGFFxQxbEzoA9ShBPO_fh-qoXUPFOWs4YI'
 URL_COINS = 'http://backend:8000/api/coins/'
 URL_MONITORED_COINS = 'http://backend:8000/api/monitoredcoins/'
